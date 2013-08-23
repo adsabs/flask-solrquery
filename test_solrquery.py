371abe21a8c94eb0c17cc2974b7a2f8a3365edbd
Jay Luker <jluker@cfa.harvard.edu>
2013-08-23 15:44:27 -0400

    successfully faking solr response; tests passing

--- test_solrquery.py
+++ test_solrquery.py
@@ -5,7 +5,8 @@ import time
 import random
 import string
 from copy import deepcopy
-from mock import Mock, patch
+from mock import patch
+from contextlib import contextmanager
 
 FIXTURES_OK = True
 try:
@@ -22,9 +23,9 @@ if sys.version_info < (2,7):
 else:
     import unittest
 
-class FlaskSolrTestCase(unittest.TestCase):
+class FakeSolrHttpResponse(object):
     
-    DEFAULT_RESPONSE = {
+    DEFAULT_DATA = {
              "responseHeader":{
                "status":0,
                "QTime":1,
@@ -47,6 +48,25 @@ class FlaskSolrTestCase(unittest.TestCase):
                         { "id": 12 }, 
                         { "id": 13 }, 
             ]}}
+    def __init__(self, data=None, status_code=200):
+        self.status_code = status_code
+        self.data = data
+        if self.data is None:
+            self.data = self.DEFAULT_DATA
+
+    def json(self):
+        return self.data
+
+@contextmanager
+def fake_solr_http_response(data=None, status_code=200):
+    def fake_send(*args, **kwargs):
+        return FakeSolrHttpResponse(data, status_code)
+    mocked_send = patch("flask_solrquery.requests.sessions.Session.send", fake_send)
+    mocked_send.start()
+    yield
+    mocked_send.stop()
+            
+class FlaskSolrTestCase(unittest.TestCase, fixtures.TestWithFixtures):
     
     def setUp(self):
         app = Flask(__name__, template_folder=os.path.dirname(__file__))
@@ -64,15 +84,18 @@ class FlaskSolrTestCase(unittest.TestCase):
         
     def test_00_query(self):
 
-        req = self.solr.create_request("black holes")
-        resp = self.solr.get_response(req)
-        self.assertEqual(resp.get_hits(), 13)
+        with fake_solr_http_response():
+            req = self.solr.create_request("black holes")
+            resp = self.solr.get_response(req)
+            self.assertEqual(resp.get_hits(), 13)
         
     def test_01_reqest_context(self):
         
         with self.app.test_request_context('/'):
-            resp = solr.query("black holes")
-            self.assertEqual(resp.get_hits(), 13)
+            with fake_solr_http_response():
+                resp = solr.query("black holes")
+                self.assertEqual(resp.get_hits(), 13)
+        
         
 if __name__ == '__main__':
     unittest.main()
